4e1aeeb7d1bb0da5b1a14e233ec2fc7bd4bac1eb
Christa Stephens <christastephens@quickenloans.com>
2019-10-09 14:30:28 -0400

    readme

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you " + name);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CSharpApp
+Goals of the app:
+Start simple with simple prompts
+that increase in complexity.
+Will use arrays, loops

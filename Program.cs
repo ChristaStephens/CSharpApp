5d422919d1ca66a17a24e72dd5a27841523fed92
Christa Stephens <christastephens@quickenloans.com>
2019-10-11 16:29:59 -0400

    added enum, need to add loop for choosing correct number

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
 ﻿using System;
 
+enum Zodiac
+{
+    aquarius = 0,
+    pisces = 1,
+    aries = 2,
+    taurus = 3,
+    gemini = 4,
+    cancer = 5,
+    leo = 6,
+    virgo = 7,
+    libra = 8,
+    scorpio = 9,
+    sagittarius =10,
+    capricorn = 11,
+
+}
 namespace csharp
 {
     class Program
@@ -9,7 +25,10 @@ namespace csharp
             Console.WriteLine("Enter your name");
             string name = Console.ReadLine();
             Console.WriteLine("Nice to meet you " + name);
-
+            Console.WriteLine("Pick a number between 0-12."); // loop here to have user try to put a number between 0-12
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(name + " based on this number your zodiac may be " + num);
+        Console.WriteLine(Zodiac.aquarius); // for reference https://www.tutorialsteacher.com/codeeditor?cid=cs-4DtLXp
         }
     }
 }
---
+++ enum.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace csharp
+{
+enum Zodiac
+{
+    aquarius = 0,
+    pisces = 1,
+    aries = 2,
+    taurus = 3,
+    gemini = 4,
+    cancer = 5,
+    leo = 6,
+    virgo = 7,
+    libra = 8,
+    scorpio = 9,
+    sagittarius =10,
+    capricorn = 11,
+
+
+
+}
+
+}

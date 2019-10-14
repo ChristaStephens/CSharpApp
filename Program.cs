34ce533608c2b6177740f2362d7685eb6c32734b
Christa Stephens <christastephens@quickenloans.com>
2019-10-14 10:00:28 -0400

    created a while loop for answering question

--- Program.cs
+++ Program.cs
@@ -22,13 +22,23 @@ namespace csharp
     {
         static void Main(string[] args)
         {
+            string finish;
+            do
+            {
+
             Console.WriteLine("Enter your name");
             string name = Console.ReadLine();
             Console.WriteLine("Nice to meet you " + name);
             Console.WriteLine("Pick a number between 0-12."); // loop here to have user try to put a number between 0-12
             int num = int.Parse(Console.ReadLine());
             Console.WriteLine(name + " based on this number your zodiac may be " + num);
-        Console.WriteLine(Zodiac.aquarius); // for reference https://www.tutorialsteacher.com/codeeditor?cid=cs-4DtLXp
+            Console.WriteLine(Zodiac.aquarius); // for reference https://www.tutorialsteacher.com/codeeditor?cid=cs-4DtLXp
+
+            Console.WriteLine("Would you like enter another number " + name + "?" + " Please enter Yes or No.");
+
+            finish = Console.ReadLine();
+            }while(finish.ToLower() != "no");
+            Console.WriteLine("Thank you for using the Home Calculator!");
         }
     }
 }

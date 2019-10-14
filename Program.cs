08f945561558dfaecfdd31a0f0342c01e4877b02
Christa Stephens <christastephens@quickenloans.com>
2019-10-14 10:54:24 -0400

    added do while and if else statement

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ enum Zodiac
     virgo = 7,
     libra = 8,
     scorpio = 9,
-    sagittarius =10,
+    sagittarius = 10,
     capricorn = 11,
 
 }
@@ -26,18 +26,28 @@ namespace csharp
             do
             {
 
-            Console.WriteLine("Enter your name");
-            string name = Console.ReadLine();
-            Console.WriteLine("Nice to meet you " + name);
-            Console.WriteLine("Pick a number between 0-12."); // loop here to have user try to put a number between 0-12
-            int num = int.Parse(Console.ReadLine());
-            Console.WriteLine(name + " based on this number your zodiac may be " + num);
-            Console.WriteLine(Zodiac.aquarius); // for reference https://www.tutorialsteacher.com/codeeditor?cid=cs-4DtLXp
+                Console.WriteLine("Enter your name");
+                string name = Console.ReadLine();
+                Console.WriteLine("Nice to meet you " + name);
+                Console.WriteLine("Pick a number between 0-12."); // loop here to have user try to put a number between 0-12
+                int num = int.Parse(Console.ReadLine());
+                if (num >= 1 && num <= 12)
+                {
 
-            Console.WriteLine("Would you like enter another number " + name + "?" + " Please enter Yes or No.");
+                    Console.WriteLine(name + " based on this number your zodiac may be " + num);
+                }
+                else
 
-            finish = Console.ReadLine();
-            }while(finish.ToLower() != "no");
+                {
+                    Console.WriteLine("Pick a number between 0-12 " + name);
+                }
+                // Console.WriteLine(Zodiac.aquarius); // for reference https://www.tutorialsteacher.com/codeeditor?cid=cs-4DtLXp
+
+                Console.WriteLine("Would you like enter another number " + name + "?" + " Please enter Yes or No.");
+
+                finish = Console.ReadLine();
+
+            } while (finish.ToLower() != "no");
             Console.WriteLine("Thank you for using the Home Calculator!");
         }
     }
